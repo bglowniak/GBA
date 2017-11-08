dcd785d32e5b610a8834e489b775c03a404fab79
Brian Glowniak <bglowniak@gmail.com>
2017-11-07 21:03:59 -0800

    Added images for square and circle. Implemented simple state machine for game state.

---
+++ characters.h
@@ -0,0 +1,30 @@
+/*
+ * Exported with nin10kit v1.7
+ * Invocation command was nin10kit --mode=3 characters square_dude.png circle_enemy.png 
+ * Time-stamp: Tuesday 11/07/2017, 20:49:42
+ * 
+ * Image Information
+ * -----------------
+ * square_dude.png 7@7
+ * circle_enemy.png 7@7
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef CHARACTERS_H
+#define CHARACTERS_H
+
+extern const unsigned short square_dude[49];
+#define SQUARE_DUDE_SIZE 98
+#define SQUARE_DUDE_LENGTH 49
+#define SQUARE_DUDE_WIDTH 7
+#define SQUARE_DUDE_HEIGHT 7
+
+extern const unsigned short circle_enemy[49];
+#define CIRCLE_ENEMY_SIZE 98
+#define CIRCLE_ENEMY_LENGTH 49
+#define CIRCLE_ENEMY_WIDTH 7
+#define CIRCLE_ENEMY_HEIGHT 7
+
+#endif
+
--- main.c
+++ main.c
@@ -4,8 +4,38 @@
 
 extern unsigned short *videoBuffer;
 
+enum GBAState {
+	START,
+	START_NODRAW,
+	GAME_OVER,
+	GAME_OVER_NODRAW
+};
+
 int main() {
 	REG_DISPCTL = MODE3 | BG2_ENABLE;
-	drawSplashScreen();
-	while(1);
+	enum GBAState state = START;
+	while(1) {
+		waitForVblank();
+		switch(state) {
+			case START:
+				drawSplashScreen();
+				state = START_NODRAW;
+				break;
+			case START_NODRAW:
+				if (KEY_DOWN_NOW(BUTTON_SELECT)) {
+					state = GAME_OVER;
+				}
+				break;
+			case GAME_OVER:
+				drawRect(0, 0, 160, 240, BLACK);
+				drawString(76, 90, "Game Over!", YELLOW);
+				state = GAME_OVER_NODRAW;
+				break;
+			case GAME_OVER_NODRAW:
+				if (KEY_DOWN_NOW(BUTTON_A)) {
+					state = START;
+				}
+				break;
+		}
+	}
 }
